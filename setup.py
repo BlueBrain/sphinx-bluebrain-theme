57d4c10b1e19ec7dc52bf929a9ad794ea31aa98d
Andrew Hale <43584291+haleepfl@users.noreply.github.com>
2020-04-21 11:04:43 +0200

    Remove build as part of sdist

--- setup.py
+++ setup.py
@@ -1,19 +1,6 @@
 #!/usr/bin/env python
 """setup.py for theme."""
 
-import subprocess
-
 from setuptools import setup
-from setuptools.command.sdist import sdist
 
-
-class BuildPy(sdist):
-    """Build the theme on sdist."""
-
-    def run(self):
-        """Run the build process."""
-        subprocess.call(("python", "translate_templates.py"))
-        sdist.run(self)
-
-
-setup(use_scm_version={"write_to": "src/version.py"}, cmdclass={"sdist": BuildPy})
+setup(use_scm_version={"write_to": "src/version.py"})
