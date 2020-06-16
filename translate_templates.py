5ee5b9c168aecb42489149ceb9d7cbd6e171d2bb
Andrew Hale <43584291+haleepfl@users.noreply.github.com>
2020-06-16 10:51:21 +0200

    Keep mkdocs-material license up-to-date

--- translate_templates.py
+++ translate_templates.py
@@ -2,6 +2,7 @@
 
 import os
 import os.path as osp
+from pathlib import Path
 
 from distutils import dir_util  # pylint: disable= no-name-in-module
 
@@ -46,27 +47,8 @@ if __name__ == "__main__":
         # do general replacements
         '{% include "assets/': '{% include "static/'
     }
-    LICENSE_TEXT = (
-        "Copyright (c) 2016-2019 Martin Donath <martin.donath@squidfunk.com>",
-        "",
-        "Permission is hereby granted, free of charge, to any person obtaining a copy",
-        'of this software and associated documentation files (the "Software"), to',
-        "deal in the Software without restriction, including without limitation the",
-        "rights to use, copy, modify, merge, publish, distribute, sublicense, and/or",
-        "sell copies of the Software, and to permit persons to whom the Software is",
-        "furnished to do so, subject to the following conditions:",
-        "",
-        "The above copyright notice and this permission notice shall be included in",
-        "all copies or substantial portions of the Software.",
-        "",
-        'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR',
-        "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,",
-        "FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE",
-        "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER",
-        "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING",
-        "FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS",
-        "IN THE SOFTWARE.",
-    )
+    LICENSE_PATH = Path(PWD_PATH) / "mkdocs-material" / "LICENSE"
+    LICENSE_TEXT = LICENSE_PATH.read_text().splitlines()
     FILES_NOT_NEEDING_LICENSE = {
         "font-awesome.css",  # license information already included
         "material-icons.css",  # license information already included
