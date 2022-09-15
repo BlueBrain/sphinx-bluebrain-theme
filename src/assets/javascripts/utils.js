562adddd8a7933bfa0d2fbf547a860ed1b649d48
Stefano Antonel <stefanoantonel@gmail.com>
2022-09-15 10:11:58 +0200

    Remove home button if not in epfl.ch/documentation

--- src/assets/javascripts/utils.js
+++ src/assets/javascripts/utils.js
@@ -1,26 +1,30 @@
 
 (() => {
-  // find the first part of path after origin
-  const firstPartReg = new RegExp("^\/([^\/]+).*$");
+
+  /*
+    Script to toggle show or hide the 'home' button depending if the it runs in
+    our internal documentation or external such as ReadTheDocs (in which case the home makes no sense)
+
+    Documentation could be served under (epfl.ch + documentation is always there)
+    https://bbp.epfl.ch/documentation/all.html
+    https://bbpteam.epfl.ch/documentation/all.html
+  */
+  const bbpOrigin = "epfl.ch";
   const bbpCatalogBase = "documentation";
-  const readthedocsOrigin = "readthedocs.io";
+  const bbpDocReg = new RegExp(`${bbpOrigin}\/${bbpCatalogBase}`);
+
+  const homeBtnContainerId = "homeBtnContainer";
   const homeButtonId = "homepageLink";
 
-  function processHomeUrl() {
-    const prunedPath = firstPartReg.exec(window.location.pathname);
-    if (prunedPath.length < 2) { return "#"; }
-
-    const basePath = prunedPath[1];
-    if (basePath === bbpCatalogBase) {
-      return `${window.location.origin}/${basePath}`;
-    }
-    if (window.location.host.includes(readthedocsOrigin)) {
-      return `${window.location.origin}`; // go to root
-    }
-    return "#";
+  if (!bbpDocReg.test(window.location.href)) {
+    const homeContainer = document.getElementById(homeBtnContainerId);
+    if (!homeContainer) { return; }
+    homeContainer.style.display = "none";
+    return;
   }
 
-  const redirectUrl = processHomeUrl();
+  const redirectUrl = `${window.location.origin}/${bbpCatalogBase}`;
   const anchorElem = document.getElementById(homeButtonId);
+  if (!anchorElem) { return; }
   anchorElem.href = redirectUrl;
 })();
