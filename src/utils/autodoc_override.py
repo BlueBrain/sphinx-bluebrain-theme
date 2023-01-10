2b09ef69669b1c813c4607848d4dd019baf2065d
Mike Gevaert <michael.gevaert@epfl.ch>
2023-01-10 11:10:13 +0100

    Update for sphinx v6.1.2
    * allow missing documentor overrides

--- src/utils/autodoc_override.py
+++ src/utils/autodoc_override.py
@@ -23,21 +23,37 @@ def add_autodoc_override(app):
         # this will override the normal autodoc directive if required. See:
         # https://github.com/sphinx-doc/sphinx/blob/master/sphinx/ext/autodoc/__init__.py#L1464
         # https://github.com/sphinx-doc/sphinx/blob/15bc5a32bb0b78b432803e55fffa10c801182c75/sphinx/application.py#L996
-        documenters = [
-            sea.ModuleDocumenter,
-            sea.ClassDocumenter,
-            sea.ExceptionDocumenter,
-            sea.DataDocumenter,
-            sea.NewTypeDataDocumenter,
-            sea.FunctionDocumenter,
-            sea.DecoratorDocumenter,
-            sea.MethodDocumenter,
-            sea.AttributeDocumenter,
-            sea.PropertyDocumenter,
-            sea.NewTypeAttributeDocumenter,
-        ]
 
-        for d in documenters:
+        # These are found by looking for class members named `objtype` in sphinx/ext/autodoc/__init__.py
+        documenters = [
+            'ModuleDocumenter',
+            'FunctionDocumenter',
+            'DecoratorDocumenter',
+            'ClassDocumenter',
+            'ExceptionDocumenter',
+            'DataDocumenter',
+            'MethodDocumenter',
+            'AttributeDocumenter',
+            'PropertyDocumenter',
+
+            # removed in https://github.com/sphinx-doc/sphinx/pull/10700/files
+            # and released in v6.1.2
+            'NewTypeDataDocumenter',
+            'NewTypeAttributeDocumenter',
+        ]
+        allowed_missing = {
+            'NewTypeDataDocumenter',
+            'NewTypeAttributeDocumenter',
+            }
+
+        for documentor_name in documenters:
+            try:
+                documentor = getattr(sea, documentor_name)
+            except AttributeError as e:
+                if documentor_name in allowed_missing:
+                    continue
+                raise e
+
             app.add_directive(
-                "auto" + d.objtype, AutodocDirectiveOverride, override=True
+                "auto" + documentor.objtype, AutodocDirectiveOverride, override=True
             )
