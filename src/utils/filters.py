2c3480c86657bf7b83a878db80a9adf1206639f1
Gianluca Ficarelli <gianluca.ficarelli@epfl.ch>
2023-08-07 11:32:25 +0200

    Fix rtd build, update requirements

--- src/utils/filters.py
+++ src/utils/filters.py
@@ -1,6 +1,6 @@
 """This module contains custom filters for the translation from mkdocs to sphinx."""
 
-from jinja2 import contextfilter
+from jinja2 import pass_context
 
 
 def add_filters(app):
@@ -12,7 +12,7 @@ def add_filters(app):
     app.builder.templates.environment.filters["url"] = url_filter
 
 
-@contextfilter
+@pass_context
 def url_filter(context, path):
     """Returns the path to static content allowing for the 'assets/' path used by mkdocs.
 
@@ -20,7 +20,7 @@ def url_filter(context, path):
 
     Args:
         context (dict): The Jinja context for the page being rendered. This is
-            given by Jinja due to the use of the @contextfilter decorator.
+            given by Jinja due to the use of the @pass_context decorator.
         path (str): A path to static (or other) content.
 
     Returns:
